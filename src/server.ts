036434fd94aa8da93bc144b38d518be2dcc4cc93
xiao <194778728+x-cean@users.noreply.github.com>
2026-01-09 17:55:28 +0100

    Fix module import to relative paths of highlights routes

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { databasePlugin } from "./core/database/database.plugin";
 import { postsRoutes } from "./modules/posts/posts.routes";
 import { reelsRoutes } from "./modules/reels/reels.routes";
 import { taggedRoutes } from "./modules/tagged/tagged.routes";
-import { highlightsRoutes } from "src/modules/highlights/highlights.routes";
+import { highlightsRoutes } from "./modules/highlights/highlights.routes";
 import multipart from "@fastify/multipart";
 import fastifyStatic from "@fastify/static";
 import path from "path";
